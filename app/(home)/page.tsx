1dcf3213696c072ebcddd44a436f948e306dde59
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-20 20:53:21 +0900

    feat: 서비스 배너 구현

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,12 @@
+import Image from 'next/image';
+
 export default function Home() {
-  return <div className='w-full text-blue-400'>Main Page</div>;
+  return (
+    <div className='flex w-full flex-col items-center'>
+      <div className='relative mt-12 h-60 w-2/3'>
+        <Image src='/banner.webp' alt='banner' fill className='rounded-2xl' />
+      </div>
+      <div className='mt-16 w-full'></div>
+    </div>
+  );
 }
