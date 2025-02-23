0219a9c19e1800a066d04c25c32706ed09f0e3ad
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-23 16:42:35 +0900

    feat: searchBar 컴포넌트에 onChange를 통해 입력한 단어 적용 구현

--- src/features/club/ui/SearchBar.tsx
+++ src/features/club/ui/SearchBar.tsx
@@ -1,15 +1,22 @@
 import { FaSearch } from 'react-icons/fa';
 
-export default function SearchBar() {
+interface searchBarProps {
+  searchTerm: string;
+  setSearchTerm: (searchTerm: string) => void;
+}
+
+export default function SearchBar({ searchTerm, setSearchTerm }: searchBarProps) {
   return (
     <div className='flex w-5/6 items-center justify-between rounded-lg border border-sky-400 bg-neutral-50 px-2 py-1'>
       <input
         type='text'
         placeholder='동아리 이름'
         className='w-full rounded-lg bg-transparent p-3 text-lg font-semibold outline-none lg:text-xl'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
       <button className='pr-4 hover:text-neutral-600'>
-        <FaSearch size={30} />
+        <FaSearch size={25} color='gray' />
       </button>
     </div>
   );
