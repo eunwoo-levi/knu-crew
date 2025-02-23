92aceb439c613e7baa32ebbb68662536598a1fe1
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-23 19:07:04 +0900

    fix: Link CSS 오류 수정

--- src/widgets/navbar/ui/Navbar.tsx
+++ src/widgets/navbar/ui/Navbar.tsx
@@ -6,8 +6,8 @@ export default function Navbar() {
   return (
     <nav className='fixed left-0 top-0 z-50 flex h-[70px] w-full items-center justify-evenly bg-white shadow-md'>
       <div className='flex justify-start'>
-        <Link href='/' className='hover:scale-1107 duration-200'>
-          <h1 className='text-3xl font-bold text-red-500'>KREW</h1>
+        <Link href='/' className='duration-200 hover:scale-110'>
+          <h1 className='text-3xl font-bold text-red-500'>KNU CREW</h1>
         </Link>
       </div>
       <div className='flex gap-10'>
