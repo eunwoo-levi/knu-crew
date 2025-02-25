eb0f516b62191fa9a31c6bf5c83b075ca8885b6a
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-26 02:09:45 +0900

    refactor: recruit 타입 변경 적용

--- src/features/club/main/ui/RecruitButton.tsx
+++ src/features/club/main/ui/RecruitButton.tsx
@@ -3,8 +3,8 @@
 import { useEffect, useRef, useState } from 'react';
 
 interface RecruitButtonProps {
-  selectedRecruit: string;
-  setSelectedRecruit: (value: string) => void;
+  selectedRecruit: boolean | null;
+  setSelectedRecruit: (value: boolean | null) => void;
 }
 
 export default function RecruitButton({ selectedRecruit, setSelectedRecruit }: RecruitButtonProps) {
@@ -29,7 +29,11 @@ export default function RecruitButton({ selectedRecruit, setSelectedRecruit }: R
     };
   }, [isModalOpen]);
 
-  const options: string[] = ['전체', '모집중', '마감모집'];
+  const options: { label: string; value: boolean | null }[] = [
+    { label: '전체', value: null },
+    { label: '모집중', value: true },
+    { label: '마감모집', value: false },
+  ];
 
   return (
     <div className='relative'>
@@ -45,19 +49,19 @@ export default function RecruitButton({ selectedRecruit, setSelectedRecruit }: R
           className='absolute right-0 top-12 w-40 rounded-lg bg-white p-2 shadow-md'
         >
           {options.map((option) => (
-            <div key={option} className='mb-2 flex justify-center'>
+            <div key={option.label} className='mb-2 flex justify-center'>
               <label className='flex w-full cursor-pointer items-center rounded-lg p-2 hover:bg-gray-100'>
                 <input
                   type='radio'
                   name='recruit'
-                  value={option}
-                  checked={selectedRecruit === option}
+                  value={option.label}
+                  checked={selectedRecruit === option.value}
                   onChange={() => {
-                    setSelectedRecruit(option);
+                    setSelectedRecruit(option.value);
                     setIsModalOpen(false);
                   }}
                 />
-                <span className='ml-2'>{option}</span>
+                <span className='ml-2'>{option.label}</span>
               </label>
             </div>
           ))}
